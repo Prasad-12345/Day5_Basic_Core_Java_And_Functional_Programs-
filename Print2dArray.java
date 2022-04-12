12ececf6209cbe6516dbc4640a87234d267382d6
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 14:50:14 +0530

    Added: Program to take array elements from user and print 2d array

---
+++ Print2dArray.java
@@ -0,0 +1,15 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to take array elements from user and print 2d array
+ */
+import utility.Util;
+
+public class Print2dArray {
+
+	public static void main(String[] args) {
+		Util.printArray();
+
+	}
+
+}
