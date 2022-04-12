c1af10f5bfcdb827b728f7616f8c572f4be67527
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 14:44:02 +0530

    Added: Program to take command line argument n and print table of power of 2 that are less than   or equal to 2^n

---
+++ PowerOf2.java
@@ -0,0 +1,18 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to take command line argument n and print table of power of 2 that are less than 
+ * or equal to 2^n
+ */
+import java.util.Scanner;
+
+import utility.Util;
+
+public class PowerOf2 {
+
+	public static void main(String[] args) {
+		
+        Util.powerOf2();
+	}
+
+}
