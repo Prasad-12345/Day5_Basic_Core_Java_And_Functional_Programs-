e9ea2d2a2029bb059dd2c2b20d4162d955406c0b
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 15:08:40 +0530

    Added: Program to find the effective temperature (the wind chill) to be

---
+++ WindChill.java
@@ -0,0 +1,15 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to find the effective temperature (the wind chill) to be 
+ */
+import utility.Util;
+
+public class WindChill {
+
+	public static void main(String[] args) {
+		
+		Util.windChill();
+	}
+
+}
