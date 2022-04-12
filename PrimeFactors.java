a5529962cdfb3f11ffc8eafc523084a7098c10dd
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 14:45:52 +0530

    Added: Program to compute the prime factorization of n and print the prime factors of n

---
+++ PrimeFactors.java
@@ -0,0 +1,17 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to compute the prime factorization of n and print the prime factors of n
+ */
+import java.util.Scanner;
+
+import utility.Util;
+
+public class PrimeFactors {
+
+	public static void main(String[] args) {
+		
+        Util.primeFactors();
+	}
+
+}
