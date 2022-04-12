45abca5ccb309ad7b586d8f587406e140f708e64
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 15:05:56 +0530

    Added: Program to swap two numbers

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to swap two numbers
+ */
+import java.util.Scanner;
+
+import utility.Util;
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+
+		Util.swapping();
+	}
+
+}
