683826f0bba56080e9554bd9ee68e65bb35ab534
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 14:33:00 +0530

    Added: Program to flip coin and print percetage of heads and tails

---
+++ FlipCoin.java
@@ -0,0 +1,16 @@
+package com.bridgelab.assignmentday5;
+
+import utility.Util;
+
+/*
+ * Author: Prasad
+ * Program to flip coin and print percetage of heads and tails
+ */
+public class FlipCoin {
+
+	public static void main(String[] args) {
+		
+		Util.headvsTail();
+	}
+
+}
