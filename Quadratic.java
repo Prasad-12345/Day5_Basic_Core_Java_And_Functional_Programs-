ff289b6a31015834a6e37f0f6f664e63118e0468
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 15:02:08 +0530

    Added: Program to find the roots of quadratic equation

---
+++ Quadratic.java
@@ -0,0 +1,15 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to find the roots of quadratic equation
+ */
+import utility.Util;
+
+public class Quadratic {
+
+	public static void main(String[] args) {
+		
+		Util.getRoots();
+	}
+
+}
