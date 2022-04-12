88cd0a71e553f0fde6ab6b4ac3bd25a59c805b1b
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 14:36:23 +0530

    Added:  Program to calculate Euclidean distance from the point (x, y) to the origin (0, 0)

---
+++ Distance.java
@@ -0,0 +1,16 @@
+package com.bridgelab.assignmentday5;
+
+import utility.Util;
+
+/*
+ * Author: Prasad
+ * Program to calculate Euclidean distance from the point (x, y) to the origin (0, 0)
+ */
+public class Distance {
+
+	public static void main(String[] args) {
+		
+		Util.distance();
+	}
+
+}
