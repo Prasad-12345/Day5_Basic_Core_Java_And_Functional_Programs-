4c14edb96c0f922607f974a7137cde51fa4a3509
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 15:07:21 +0530

    Added:  Program to check entered alphabet is vowel or consonant

---
+++ VowelOrConsonant.java
@@ -0,0 +1,16 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to check weather an alphabet is vowel or consonant
+ */
+import java.util.Scanner;
+
+import utility.Util;
+
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+	
+		Util.checkVowelOrConsonant();
+}
+}
