5fea211d125879e995fd97476e5581199e2f1b03
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 15:03:17 +0530

    Added: Program to find quotient and remainder

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,17 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to find quotient and remainder
+ */
+import java.util.Scanner;
+
+import utility.Util;
+
+public class QuotientAndRemainder {
+
+	public static void main(String[] args) {
+    
+		Util.quotientAndRemainder();
+	}
+
+}
