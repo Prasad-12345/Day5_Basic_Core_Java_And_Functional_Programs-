91752b6a26d3bff819e5adf723cdd927ded830f1
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 14:37:43 +0530

    Program to check weather a number is even or odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to check weather a number is even or odd
+ */
+import java.util.Scanner;
+
+import utility.Util;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+
+		Util.evenOdd();
+	}
+
+}
