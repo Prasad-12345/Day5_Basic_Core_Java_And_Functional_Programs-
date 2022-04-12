20cecc0cd2638c09ecbc07d3cd0c6fe2485a5985
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 14:40:49 +0530

    Added: Program to find largest among three numbers

---
+++ LargestAmongThree.java
@@ -0,0 +1,17 @@
+package com.bridgelab.assignmentday5;
+/*
+ * Author: Prasad
+ * Program to find largest among three numbers
+ */
+import java.util.Scanner;
+
+import utility.Util;
+
+public class LargestAmongThree {
+
+	public static void main(String[] args) {
+
+		Util.largestNumber();
+	}
+
+}
